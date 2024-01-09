2c2cf079877e94bf354b36c38cdfacb7d7796197
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-01-09 13:38:50 +0530

    number square

---
+++ numbersquare.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the side of square:");
+    scanf("%d",&n);
+    //nested loops
+    for(int i=1;i<=n;i++) //rows
+    {
+        for(int j=1;j<=n;j++) //columns
+        {
+            printf("%d ",i);//it can be j also
+        }
+        printf("\n");
+    }
+    return 0;
+}
