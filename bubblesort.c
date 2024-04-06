f30979adf6403cbf74de723844666255f6eca3e1
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-04-06 07:57:52 +0530

    Add files via upload

---
+++ bubblesort.c
@@ -0,0 +1,26 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int arr[]={5,6,9,10,15,4,2,3,1};
+    int n=9;
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){
+                arr[j]=arr[j]^arr[j+1];
+                arr[j+1]=arr[j]^arr[j+1];
+                arr[j]=arr[j]^arr[j+1];
+                for(int i=0;i<n;i++){
+                    printf("%d\t",arr[i]);
+                }   
+                printf("\n");
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        printf("%d\t",arr[i]);
+    }
+    
+
+    return 0;
+}
