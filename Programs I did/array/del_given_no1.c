7211c6dcb549ada7867df3556d509f8cba558386
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-04-13 16:00:43 +0530

    Update del_given_no1.c

--- Programs I did/array/del_given_no1.c
+++ Programs I did/array/del_given_no1.c
@@ -1,35 +1,21 @@
 #include<stdio.h>
-/*
- *C program to accept an array of integers and delete the specified 
- *integer from the list repeated and consecutive numbers
- */
-
 int main(){
     int vectorx[10];
     int i,j,pos,element,found=0;
     for(i=0;i<10;i++)
-    {
         printf("\nEnter the number:");
         scanf("%d",&vectorx[i]);
-    }
     printf("Enter the element to be deleted\n");
-
     scanf("%d",&element);
     found=9;
     for(i=0;i<=found;i++)
-    {
         if(vectorx[i]==element)
-        {
             for(j=i+1;j<=found;j++)
-            vectorx[j-1]=vectorx[j];
-            i--;
-            found--;
-        }
-    }
+                vectorx[j-1]=vectorx[j];
+                i--;
+                found--;
     printf("The resultant vector is \n",element);
     for(i=0;i<=found;i++)
-    {
         printf("%d\n",vectorx[i]);
-    }
     return 0;
 }
