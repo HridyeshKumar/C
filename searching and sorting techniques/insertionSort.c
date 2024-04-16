435dab99cd3ce8e2565d438eebe5c8372671e4ca
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-04-16 16:29:37 +0530

    Update insertionSort.c

--- searching and sorting techniques/insertionSort.c
+++ searching and sorting techniques/insertionSort.c
@@ -4,21 +4,17 @@ int main(){
     printf("Enter the number of elements:\n");
     scanf("%d",&n);
     printf("Enter %d integers:\n",n);
-    for(c=0;c<n;c++){
+    for(c=0;c<n;c++)
         scanf("%d",&array[c]);
-    }
-    for(c=1;c<=n-1;c++){
+    for(c=1;c<=n-1;c++)
         d=c;
-        while(d>0 &&array[d]<array[d-1]){
+        while(d>0 &&array[d]<array[d-1])
             t=array[d];
             array[d]=array[d-1];
             array[d-1]=t;
             d--;
-        }
-    }
     printf("Sorted list in ascending order:\n");
-    for(c=0;c<=n-1;c++){
+    for(c=0;c<=n-1;c++)
         printf("%d\n",array[c]);
-    }
     return 0;
 }
