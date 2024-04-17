65933622773e3e2448550446e216eceef9fa425b
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-04-17 21:48:10 +0530

    Update alfa_num_triangle.c

--- Programs I did/pattern printing/alfa_num_triangle.c
+++ Programs I did/pattern printing/alfa_num_triangle.c
@@ -9,7 +9,7 @@ int main(){
         for(int j=1;j<=i;j++) //columns
         {
             if(i%2==0)
-            printf("%d ",j);//it can be i also
+            printf("%d ",j);
             else
             printf("%c ",j+64);
         }
