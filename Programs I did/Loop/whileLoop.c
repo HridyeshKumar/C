e20a2f1a15fc806ef30cc5b3bea370900b2c3c73
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-07-21 09:07:18 +0530

    Create whileLoop.c

---
+++ Programs I did/Loop/whileLoop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Don't forget to declare variables
+
+    int i = 0;
+
+    // While i is less than 10
+    while (i < 10)
+    {
+
+        printf("%d\n", i);
+        // Update i so the condition can be met eventually
+        i++;
+    }
+    return 0;
+}
