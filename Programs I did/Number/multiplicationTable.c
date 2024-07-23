3930fd7dcc99049056dbdd66aeaeb8355bbf37ef
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-07-23 07:32:52 +0530

    Update Number

---
+++ Programs I did/Number/multiplicationTable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, i;
+
+    printf("Enter an integer to find multiplication table: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= 10; ++i)
+    {
+        printf("%d * %d = %d\n", n, i, n * i);
+    }
+
+    return 0;
+}
