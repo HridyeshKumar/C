fc98f951516e19b090a4cd724680dceac0868a4f
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-07-24 14:19:01 +0530

    Update Miscellaneous

---
+++ Programs I did/Miscellaneous/shutdownUbuntoLinux.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+   char ch;
+ 
+   printf("Do you want to shutdown your computer now (y/n) : ");
+   scanf("%c", &ch);
+ 
+   if (ch == 'y' || ch == 'Y')
+      system("shutdown -P now");
+ 
+   return 0;
+}
