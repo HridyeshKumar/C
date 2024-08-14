fb79b19a533083eafe1ded26406462c9725000f4
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-08-14 14:30:05 +0530

    Create str_len.c

---
+++ strings/str_len.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int string_ln(char*);
+void main()
+{
+    char str[20];
+    int length;
+    printf("\nEmter the string:");
+    gets(str);
+    length=string_ln(str);
+    printf("The length of the given string %s is %d",str,length);
+}
+int string_ln(char*p)
+{
+    int count=0;
+    while(*p!='\0')
+    {
+        count++;
+        p++;
+    }
+    return count;
+}
+
+
