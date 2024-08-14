583f873685e5e418251b670aa2efc681ac47cb46
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-08-14 23:29:27 +0530

    Create Floyd'sSquare.c

---
+++ pattern printing/Floyd'sSquare.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the side of square:");
+    scanf("%d",&n);
+    //nested loops
+    int a=1;
+    for(int i=1;i<=n;i++) //rows
+    {
+        for(int j=1;j<=n;j++) //columns
+        {
+            if(a<10){
+                printf("%d  ",a);//it can be i also
+                a++;
+            }
+            else{
+                printf("%d ",a);//it can be i also
+                a++;
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the side of square:");
+    scanf("%d",&n);
+    //nested loops
+    int a=1;
+    for(int i=1;i<=n;i++) //rows
+    {
+        for(int j=1;j<=n;j++) //columns
+        {
+            if(a<10){
+                printf("%d  ",a);//it can be i also
+                a++;
+            }
+            else{
+                printf("%d ",a);//it can be i also
+                a++;
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
