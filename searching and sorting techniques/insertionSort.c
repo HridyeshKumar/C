492b360c159f809a45126d4813dcf5b2304ad5e5
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-08-23 13:14:45 +0530

    Update insertionSort.c

--- searching and sorting techniques/insertionSort.c
+++ searching and sorting techniques/insertionSort.c
@@ -1,20 +1,25 @@
-#include<stdio.h>
-int main(){
-    int n,array[1000],c,d,t;
+#include <stdio.h>
+
+int main() {
+    int n, array[1000], c, d, t;
     printf("Enter the number of elements:\n");
-    scanf("%d",&n);
-    printf("Enter %d integers:\n",n);
-    for(c=0;c<n;c++)
-        scanf("%d",&array[c]);
-    for(c=1;c<=n-1;c++)
-        d=c;
-        while(d>0 &&array[d]<array[d-1])
-            t=array[d];
-            array[d]=array[d-1];
-            array[d-1]=t;
+    scanf("%d", &n);
+    printf("Enter %d integers:\n", n);
+    for (c = 0; c < n; c++) {
+        scanf("%d", &array[c]);
+    }
+    for (c = 1; c < n; c++) {
+        t = array[c];
+        d = c - 1;
+        while (d >= 0 && array[d] > t) {
+            array[d + 1] = array[d];
             d--;
+        }
+        array[d + 1] = t; 
+    }
     printf("Sorted list in ascending order:\n");
-    for(c=0;c<=n-1;c++)
-        printf("%d\n",array[c]);
+    for (c = 0; c < n; c++) {
+        printf("%d\n", array[c]);
+    }
     return 0;
 }
