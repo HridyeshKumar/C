a4462545cdf3af2759d1ec01b8ea50e4f513e3e6
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-08-28 16:12:30 +0530

    Update largeUsingGlobalVarDeclaration.c

--- Miscellaneous/largeUsingGlobalVarDeclaration.c
+++ Miscellaneous/largeUsingGlobalVarDeclaration.c
@@ -1,28 +1,22 @@
 #include<stdio.h>
-
 // Global variable declaration
 int a, b;
 
-int main()
-{
+int main(){
     printf("enter the two values to find the greatest number : ");
     scanf("%d%d", &a, &b);
 
     //logic to find the largest and smallest numbers
-    if (a < b)
-    {
+    if (a < b){
         printf("\nthe largest number is : %d", b);
         printf("\nthe Smallest number is : %d", a);
     }
-    else if (a == b)
-    {
+    else if (a == b){
         printf("Both are Equal");
     }
-    else
-    {
+    else{
         printf("\nthe largest number is : %d", a);
         printf("\nthe Smallest number is : %d", b);
     }
-
     return 0;
 }
