72058158364ac0c9530769cb319d1dd6665ccb8c
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-08-29 20:39:34 +0530

    Update binaryToDecimal.c

--- Formula/binaryToDecimal.c
+++ Formula/binaryToDecimal.c
@@ -2,25 +2,18 @@
 #include <math.h>
 
 int binary_decimal(int n);
-
-int main()
-{
+int main(){
     int n;
     char c;
-
     printf("Enter a binary number: ");
     scanf("%d", &n);
     printf("%d in binary = %d in decimal", n, binary_decimal(n));
-
     return 0;
 }
-
 //Function to convert binary to decimal.
-int binary_decimal(int n)
-{
+int binary_decimal(int n){
     int decimal = 0, i = 0, rem;
-    while (n != 0)
-    {
+    while (n != 0){
         rem = n % 10;
         n /= 10;
         decimal += rem * pow(2, i);
