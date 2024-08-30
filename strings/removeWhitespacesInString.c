686341ce4e8437eb1c09a4cafc252d619a3aaacf
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-08-30 10:12:58 +0530

    Update removeWhitespacesInString.c

--- strings/removeWhitespacesInString.c
+++ strings/removeWhitespacesInString.c
@@ -1,22 +1,17 @@
 #include <string.h>
 #include <stdio.h>
 
-int main()
-{
+int main(){
     int i = 0, j = 0, len;
     char buff[50];
     printf("Enter String with white space ");
     gets(buff);
-
     len = (int) strlen(buff);
-
-    while (i != len)
-    {
+    while (i != len){
         if (buff[i] != ' ')
             buff[j++] = buff[i];
         i++;
     }
     buff[j] = 0;
-
     printf("Your String  is %s.", buff);
 }
