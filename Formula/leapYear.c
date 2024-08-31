687aff8476c0b288c6b57714340e2c657a24cb3a
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-08-31 18:47:36 +0530

    Update leapYear.c

--- Formula/leapYear.c
+++ Formula/leapYear.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
-int main()
-{
+int main(){
     int year;
-
     printf("Enter a year to check if it is a leap year\n");
     scanf("%d", &year);
-
     if (year % 400 == 0)
         printf("%d is a leap year.\n", year);
     else if (year % 100 == 0)
@@ -15,6 +12,5 @@ int main()
         printf("%d is a leap year.\n", year);
     else
         printf("%d is not a leap year.\n", year);
-
     return 0;
 }
