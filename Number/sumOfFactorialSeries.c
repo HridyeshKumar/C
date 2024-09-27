95d290692803094049a9913f69ac1996a21c0479
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-09-27 18:44:10 +0530

    Update sumOfFactorialSeries.c

--- Number/sumOfFactorialSeries.c
+++ Number/sumOfFactorialSeries.c
@@ -2,32 +2,23 @@
 
 double sumseries(double);
 
-int main()
-{
+int main(){
     double number, sum;
     printf("Enter the value:  ");
     scanf("%lf", &number);
     sum = sumseries(number);
-
     printf("\nSum of the above series = %lf ", sum);
-    
     return 0;
 }
-
-double sumseries(double m)
-{
+double sumseries(double m){
     double sum2 = 0, f = 1, i;
-    for (i = 1; i <= m; i++)
-    {
+    for (i = 1; i <= m; i++){
         f = f * i;
         sum2 = sum2 + (i / f);
-
-        if (i == m)
-        {
+        if (i == m){
             printf("%.2lf / %.2lf = %lf", i, f, sum2);
         }
-        else
-        {
+        else{
             printf("%.2lf / %.2lf + \n", i, f);
         }
     }
