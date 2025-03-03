d3b89ece807fbce465db01426c59ff470e2706c3
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2025-03-04 02:27:29 +0530

    Add files via upload

--- array/matrix_multiplication.c
+++ array/matrix_multiplication.c
@@ -1,25 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
+int main()
+{
    int m,n,sum=0;
    int a[3][4],b[4][2],result[3][2];
    printf("Enter the first matrix\n");
    for(int i=0;i<3;i++)
-      for(int j=0;j<4;j++)
+   {
+   for(int j=0;j<4;j++)
+       {
+        // printf("Enter the %d %d element of first matrix\n",i,j);
         scanf("%d",&a[i][j]);
-   printf("Enter the second matrix\n");
-   for(int i=0;i<4;i++)
-      for(int j=0;j<2;j++)
+        //  printf("\t");
+       }
+    //    printf("\n");
+   }
+    printf("Enter the second matrix\n");
+    for(int i=0;i<4;i++)
+    {
+    for(int j=0;j<2;j++)
+       {
+        // printf("Enter the %d %d element of second matrix\n",i,j);
         scanf("%d",&b[i][j]);
+        // printf("\t");
+       } 
+    //   printf("\n");
+   }
     for(int i=0;i<3;i++)
-       for(int j=0;j<2;j++)
-          for(int k=0;k<4;k++)
+    {
+    for(int j=0;j<2;j++)
+       {
+        //calculate the result
+        for(int k=0;k<4;k++)
+        {
             sum+=a[i][k]*b[k][j];
+        }
         result[i][j]=sum;
         sum=0;
+       }
+   }
+
+   //print the resultant matrix
    for(int i=0;i<3;i++)
-      for(int j=0;j<2;j++)   
+    {
+    for(int j=0;j<2;j++)    
+       {
+            //print the result
             printf("%d \t",result[i][j]);
+        }
     printf("\n");
-    return 0;
+    }
+    
 }      
