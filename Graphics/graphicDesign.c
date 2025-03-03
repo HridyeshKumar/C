6515e26dd7cb8539349be04b0b8c133acceb35f0
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2025-03-04 02:13:58 +0530

    Add files via upload

---
+++ Graphics/graphicDesign.c
@@ -0,0 +1,18 @@
+// #include<stdio.h>
+// #include<graphics.h>
+// #include<dos.h>
+// void main()
+// {
+//     clrscr();
+//     int a=DETECT,b;
+//     int i;
+//     initgraph(&a,&b,"C:\\TC\\BGI");
+//     for(i=0;i<=100;i++)
+//     {
+//         circle(319,219-i,20+i);
+//         circle(319,219+i,20+i);
+//         circle(299-i,239,20+i);
+//         circle(339+i,239,20+i);
+//     }
+//     getch();
+// }
